09ca0b951c67c14470a93b77a5921e1de8cca757
Ilyas Mukatdisov <mukatdisovilyas@gmail.com>
2023-05-18 17:13:47 +0500

    added datePicker

--- lib/features/expense/presentation/widgets/new_expense.dart
+++ lib/features/expense/presentation/widgets/new_expense.dart
@@ -2,6 +2,10 @@ import 'package:expense_tracker/core/utils/utils.dart';
 import 'package:expense_tracker/features/expense/domain/entity/expense.dart';
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
@@ -16,6 +20,26 @@ class _NewExpenseState extends State<NewExpense> {
   double? dollarAmount = 0.0;
 
   final _debouncer = Debouncer(milliseconds: 1000);
+  DateTime? selectedDate;
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    ).then(
+      (date) {
+        setState(
+          () {
+            selectedDate = date;
+          },
+        );
+      },
+    );
+  }
 
   @override
   initState() {
@@ -74,6 +98,10 @@ class _NewExpenseState extends State<NewExpense> {
                     onPressed: _openAddCategoryOverlay,
                   ),
                 ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(category == null ? 'Add Category' : ''),
                 Expanded(
                   child: SizedBox(
                     height: 48,
@@ -109,16 +137,42 @@ class _NewExpenseState extends State<NewExpense> {
               ],
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(category == null ? 'Add Category' : ''),
                 Text(
                   dollarAmount != null
                       ? '\$${dollarAmount?.toStringAsFixed(2)}'
                       : '',
                 ),
+                const SizedBox(width: 10),
               ],
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.calendar_month),
+                  onPressed: _presentDatePicker,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text('Selected date: '),
+                selectedDate != null
+                    ? Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            formatter.format(selectedDate!),
+                            textAlign: TextAlign.end,
+                          ),
+                        ),
+                      )
+                    : Container(),
+              ],
+            )
           ],
         ),
       ),
